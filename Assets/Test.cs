c362c2260176466ac992e2fd6869df787c92b44c
musashi0510 <78544643+musashi0510@users.noreply.github.com>
2021-03-15 22:58:15 +0900

    Lesson4の課題と発展課題について
    課題と発展課題を作成

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -4,31 +4,45 @@ using UnityEngine;
 
 public class Boss
 {
-private int mp = 53;
-public void Magic()
-{
-if(mp >= 5)
-{
-int num = mp - 5;
-Debug.Log("魔法攻撃を攻撃した。残りMPは" + num);
+	private int mp = 53;
+
+	public void Magic()
+	{
+		if(mp >= 5)
+		{
+			mp = mp - 5;
+			Debug.Log("魔法攻撃を攻撃した。残りMPは" + mp);
+		}
+		else
+		{
+			Debug.Log("MPが足りないため、魔法攻撃が使えない。");
+		}
+	}
 }
-else
-{
-Debug.Log("MPが足りないため、魔法攻撃が使えない。");
-}
-}
-}
+
 public class Test : MonoBehaviour
 {
-void Start()
-{
-Boss lastboss = new Boss();
-lastboss.Magic();
-for(int i = 0;i < 10;i++)
-{
-Debug.Log("MPが足りないため、魔法攻撃が使えない。");
-}
-}
+	void Start()
+	{
+		Boss lastboss = new Boss();
+
+		for(int i = 0;i < 10;i++)
+		{
+			lastboss.Magic();
+		}
+
+		//以下にLesson4の課題を示す。
+		int[] array ={1,2,3,4,5};
+
+		for (int a = 0;a < array.Length;a++)
+		{
+			Debug.Log(array [a]);
+		}
+		for (int b = array.Length-1;b >= 0;b--)
+		{
+			Debug.Log(array [b]);
+		}
+	}
 
     // Update is called once per frame
     void Update()
